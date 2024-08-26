fb56eb4302661d65d7cbdb6ceaeba7d06c1dec90
Saptarsi Das <123920572+Saptarsi-07@users.noreply.github.com>
2024-08-26 23:50:13 +0530

    Update kth_action.py

--- core/data_provider/kth_action.py
+++ core/data_provider/kth_action.py
@@ -84,9 +84,10 @@ class ActionFrameInfo:
 class DataProcess:
     def __init__(self, input_param):
         self.paths = input_param['paths']  # path to parent folder containing category dirs
-        self.category_1 = ['boxing', 'handclapping', 'handwaving', 'walking']
-        self.category_2 = ['jogging', 'running']
-        self.categories = self.category_1 + self.category_2
+        # self.category_1 = ['boxing', 'handclapping', 'handwaving', 'walking']
+        self.categories = ['walking']
+        # self.category_2 = ['jogging', 'running']
+        # self.categories = self.category_1 + self.category_2
         self.image_width = input_param['image_width']
 
         # Hard coded training and test persons (prevent same person occurring in train - test set)
