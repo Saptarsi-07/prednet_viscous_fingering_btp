54a7c6ed8ff8a5e838ff5b37de97514658e5d5e6
Tanish Jain <jtanish120204@gmail.com>
2024-09-29 02:02:53 +0530

    Update Demo

--- saptarsi_custom/data_generator_fingering.py
+++ saptarsi_custom/data_generator_fingering.py
@@ -1,7 +1,6 @@
 import numpy as np 
 import preprocess 
 import math
-import model as predRNNv2
 import datetime # For logging into the terminal 
 import warnings 
 import os 
@@ -16,9 +15,9 @@ SIZE_TRAIN_DATA = math.ceil(SIZE_TRAIN_VALID_DATA * TRAIN_VALID_SPLIT_RATIO)
 
 print(f'Splitting data into {SIZE_TRAIN_DATA} training frames, {SIZE_TRAIN_VALID_DATA - SIZE_TRAIN_DATA} validation frames, and {len(frames) - SIZE_TRAIN_VALID_DATA} test frames')
 
-train_data = frames[:SIZE_TRAIN_DATA+1]  # Split into training, validation, and test data 
-valid_data = frames[SIZE_TRAIN_DATA+1:SIZE_TRAIN_VALID_DATA+1]
-test_data = frames[SIZE_TRAIN_VALID_DATA+1:]
+train_data = frames[:SIZE_TRAIN_DATA]  # Split into training, validation, and test data 
+valid_data = frames[SIZE_TRAIN_DATA:SIZE_TRAIN_VALID_DATA]
+test_data = frames[SIZE_TRAIN_VALID_DATA:]
 
 train_dict = dict()
 valid_dict = dict()
@@ -27,16 +26,16 @@ test_dict = dict()
 TOTAL_LENGTH = 20
 INPUT_LENGTH = 10
 
-LENGTH = SIZE_TRAIN_DATA // TOTAL_LENGTH
+LENGTH = SIZE_TRAIN_DATA//TOTAL_LENGTH
 print(LENGTH)
 
 INPUT_HEIGHT = 128
 INPUT_WIDTH = 128
-N_CHANNELS = 3 
+N_CHANNELS = 1 
 
 # Initializing the train_dict to store clips and input data # 
-train_dict['clips'] = np.zeros(shape=(2, LENGTH, 2))
-train_dict['input_raw_data'] = np.zeros(shape=(SIZE_TRAIN_DATA, N_CHANNELS, INPUT_HEIGHT, INPUT_WIDTH))
+train_dict['clips'] = np.zeros(shape=(2, LENGTH, 2), dtype=np.int32)
+train_dict['input_raw_data'] = np.zeros(shape=(SIZE_TRAIN_DATA, N_CHANNELS, INPUT_HEIGHT, INPUT_WIDTH), dtype=np.int32)
 
 for i in range(LENGTH):
     train_dict['clips'][0,i,0] = i * TOTAL_LENGTH
@@ -47,12 +46,13 @@ for i in range(LENGTH):
         train_dict['input_raw_data'][i+k] = np.reshape(train_data[i+k], (N_CHANNELS, INPUT_HEIGHT, INPUT_WIDTH))
 
 # Store dimensions in the train_dict, not in train_data
-train_dict['dims'] = [[1, INPUT_HEIGHT, INPUT_WIDTH]] # [[1, INPUT_HEIGHT, INPUT_WIDTH]]
+train_dict['dims'] = [[N_CHANNELS, INPUT_HEIGHT, INPUT_WIDTH]] # [[N_CHANNELS, INPUT_HEIGHT, INPUT_WIDTH]]
 
 # Print the stored data
 print(train_dict['dims'])
 print(train_dict['clips'].shape)
 print(train_dict['input_raw_data'].shape)
+print(train_data[0])
 
 np.savez('../fingering_train_data.npz', **train_dict)
 
--- saptarsi_custom/preprocess.py
+++ saptarsi_custom/preprocess.py
@@ -7,9 +7,8 @@ This file contains logic for greyscaling, rescaling to custom resolution (for ex
 import cv2
 import os 
 import numpy as np 
-from terminal_colors import terminal_colors
 
-def extract_frames(path_to_video):
+def preprocess(path_to_video, resolution):
     '''
         extract_frames(path_to_video)
         Extracts the frames from the video available at path. 
@@ -27,38 +26,10 @@ def extract_frames(path_to_video):
             continue 
         frames.append(frame) # append extracted frame 
         count += 1
-    return np.array(frames)
-
-def rescale_frames(frames, resolution):
-    for frame in frames: 
+    framesTemp = np.array(frames)
+    framesRes = []
+    for frame in framesTemp: 
         frame = cv2.resize(frame, tuple(resolution))
-
-def grayscale_frames(frames):
-    for frame in frames:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-def preprocess(path_to_video, resolution): 
-    '''
-        preprocess(path_to_video)
-        Preprocess the video available at path_to_video and writes the output at output_folder
-    '''
-
-    if len(resolution) != 2:
-        print(f'{terminal_colors.WARNING}Expected 2 values, got {len(resolution)}{terminal_colors.ENDC}')
-
-    frames = extract_frames(path_to_video)
-    rescale_frames(frames, resolution)
-    grayscale_frames(frames)
-
-    # if not output_folder[-1] == "\\":
-    #     output_folder = output_folder + '\\'
-    # if os.path.isdir(output_folder):
-    #     os.rmdir(output_folder)
-    # os.mkdir(output_folder)
-    # count = 0
-    # for frame in frames:
-    #     count += 1
-    #     path = output_folder + f"{count:04}" + ".jpg"
-    #     cv2.imwrite(path, frame)
-    return frames 
-
+        framesRes.append(frame)
+    return np.array(framesRes)
